82dbacf83899dfceb39fc79c521b56998a638541
SABINO KARI B <202100977b@utea.edu.pe>
2023-09-06 19:04:57 -0500

    Primer_Commit

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,25 @@ class Myapp extends StatelessWidget {
             radius: 60.0,
             backgroundImage: NetworkImage(
                 "https://images.pexels.com/photos/18165267/pexels-photo-18165267/free-photo-of-mujer-modelo-joven-cara.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"),
+            //backgroundImage: AssetImage("assets/images/mujer.jpeg"),
           ),
+          Text(
+            "Fiorella Tapia Ramirez",
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 20.0,
+              fontWeight: FontWeight.w700,
+              fontFamily: "Lobster",
+            ),
+          ),
+          Text(
+            "FLUTTER DEVELOPER",
+            style: TextStyle(
+              //color: Color.white60,
+              fontSize: 14.0,
+              letterSpacing: 2.0,
+            ),
+          )
         ],
       ),
     ));
